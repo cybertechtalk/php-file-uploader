be86581d2ac79d1d91bdd3444a55eb4e74124c18
cybertechtalk <akar.taget@gmail.com>
2023-04-15 17:51:13 +0200

    redone

--- index.php
+++ index.php
@@ -1,93 +1,53 @@
-<?php
-if (isset($_GET["source"])) 
-    die(highlight_file(__FILE__));
+<?PHP
+    $disallowed_ext = array(
+        "php",
+        "php3",
+        "php4",
+        "php5",
+        "php7",
+        "pht",
+        "phtm",
+        "phtml",
+        "phar",
+        "phps",
+        "exe",
+        "ps1"
+    );
 
-session_start();
+    if(!empty($_FILES['uploaded_file']))
+    {
+        $path = "uploads/";
+        $path = $path . basename( $_FILES['uploaded_file']['name']);
 
-if (!isset($_SESSION["home"])) {
-    $_SESSION["home"] = bin2hex(random_bytes(20));
-}
-$userdir = "images/{$_SESSION["home"]}/";
-if (!file_exists($userdir)) {
-    mkdir($userdir);
-}
+        $tmp_name = $_FILES['uploaded_file']['tmp_name'];
+        $name = $$_FILES['uploaded_file']['name'];
+        $parts = explode(".", $name);
+        $ext = array_pop($parts);
 
-$disallowed_ext = array(
-    "php",
-    "php3",
-    "php4",
-    "php5",
-    "php7",
-    "pht",
-    "phtm",
-    "phtml",
-    "phar",
-    "phps",
-    "exe",
-    "ps1"
-);
+        if (in_array($ext, $disallowed_ext, TRUE)) {
+            die("$ext is not allowed");
+        }
 
-if (isset($_POST["upload"])) {
-    if ($_FILES['image']['error'] !== UPLOAD_ERR_OK) {
-        die("upload failed");
+        if(move_uploaded_file($tmp_name, $path)) {
+        echo "The file ".  basename($name). 
+        " has been uploaded";
+        } else{
+            echo "There was an error uploading the file, please try again!";
+        }
     }
-
-    $tmp_name = $_FILES["image"]["tmp_name"];
-    $name = $_FILES["image"]["name"];
-    $parts = explode(".", $name);
-    $ext = array_pop($parts);
-
-    if (empty($parts[0])) {
-        array_shift($parts);
-    }
-
-    if (in_array($ext, $disallowed_ext, TRUE)) {
-        die("$ext is not allowed");
-    }
-
-    $image = file_get_contents($tmp_name);
-    if (mb_strpos($image, "<?") !== FALSE) {
-        die("wrong image");
-    }
-
-    $name = implode(".", $parts);
-    move_uploaded_file($tmp_name, $userdir . $name . "." . $ext);
-}
-
-echo "<h3>Your <a href=$userdir>files</a>:</h3><ul>";
-foreach(glob($userdir . "*") as $file) {
-    echo "<li><a href='$file'>$file</a></li>";
-}
-echo "</ul>";
-
 ?>
-<html lang="en">
+
+
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+  <title>Upload your files</title>
 </head>
 <body>
-    <div class="h-64 border-solid border-black border-4 text-center" id="dropzone">
-        Drop file here
-    </div>
-
-<form method="POST" action="index.php" enctype="multipart/form-data">
-    <input type="file" name="image" id="files">
-    <input type="submit" name=upload>
-</form>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script>
-        var $dropzone = $('#dropzone');
-        $dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-        }).on('drop', function(event) {
-            document.querySelector('#files').files = event.originalEvent.dataTransfer.files;
-        });
-    </script>
+  <form enctype="multipart/form-data" action="index.php" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
 </body>
 </html>
